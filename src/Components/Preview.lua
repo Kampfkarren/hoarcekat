8fc3d050431bf1ea1f6814627db10de9a02039ac
Kampfkarren <boynedmaster@gmail.com>
2020-01-19 05:05:03 -0800

    Track script updates

--- src/Components/Preview.lua
+++ src/Components/Preview.lua
@@ -1,5 +1,6 @@
 local Hoarcekat = script:FindFirstAncestor("Hoarcekat")
 
+local EventConnection = require(script.Parent.EventConnection)
 local Roact = require(Hoarcekat.Vendor.Roact)
 local RoactRodux = require(Hoarcekat.Vendor.RoactRodux)
 
@@ -26,11 +27,24 @@ function Preview:refreshPreview()
 
 	local selectedStory = self.props.selectedStory
 	if selectedStory then
-		self.cleanup = require(selectedStory:Clone())(self.previewRef:getValue())
+		-- loadstring is used to avoid cache while preserving `script` (which requiring a clone wouldn't do)
+		local story = assert(loadstring(selectedStory.Source))
+
+		local fenv = setmetatable({
+			script = selectedStory
+		}, {
+			__index = getfenv(),
+		})
+
+		setfenv(story, fenv)
+
+		self.cleanup = story()(self.previewRef:getValue())
 	end
 end
 
 function Preview:render()
+	local selectedStory = self.props.selectedStory
+
 	return e("Frame", {
 		BackgroundTransparency = 1,
 		Size = UDim2.fromScale(1, 1),
@@ -45,11 +59,35 @@ function Preview:render()
 			Size = UDim2.fromScale(1, 1),
 			[Roact.Ref] = self.previewRef,
 		}),
+
+		TrackChanged = selectedStory and e(EventConnection, {
+			callback = function()
+				self:refreshPreview()
+			end,
+			event = selectedStory:GetPropertyChangedSignal("Source"),
+		}),
+
+		TrackRemoved = selectedStory and e(EventConnection, {
+			callback = function()
+				if not selectedStory:IsDescendantOf(game) then
+					self.props.endPreview()
+				end
+			end,
+			event = selectedStory.AncestryChanged,
+		}),
 	})
 end
 
 return RoactRodux.connect(function(state)
 	return {
 		selectedStory = state.StoryPicker,
+	}
+end, function(dispatch)
+	return {
+		endPreview = function()
+			dispatch({
+				type = "SetSelectedStory",
+			})
+		end,
 	}
 end)(Preview)
