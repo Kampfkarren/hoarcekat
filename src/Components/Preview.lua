7754fb00e79f63aa026ae6cc0d665415135341f9
OverHash <46231745+OverHash@users.noreply.github.com>
2021-10-24 19:49:52 +1300

    Fix ZIndexBehaviour on expanded preview

--- src/Components/Preview.lua
+++ src/Components/Preview.lua
@@ -24,6 +24,7 @@ function Preview:init()
 
 	local display = Instance.new("ScreenGui")
 	display.Name = "HoarcekatDisplay"
+	display.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 	self.display = display
 
 	self.expand = false
