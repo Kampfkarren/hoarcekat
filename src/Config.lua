c1a9ea8de4eb7ff7005f65472c9c5d29432bd307
Nidoxs <nidoxsdevelopment@gmail.com>
2023-10-24 01:04:07 +0100

    Move `USER_SERVICES` to `Config`

--- src/Config.lua
+++ src/Config.lua
@@ -1,5 +1,14 @@
 local Config = {
 	useDevSource = false,
+	userServices = {
+		"Workspace",
+		"ReplicatedFirst",
+		"ReplicatedStorage",
+		"ServerScriptService",
+		"ServerStorage",
+		"StarterGui",
+		"StarterPlayer",
+	},
 }
 
 return Config
