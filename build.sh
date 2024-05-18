f821f387054a6ae773519dbdb2e7babbd5af0e43
Nidoxs <nidoxsdevelopment@gmail.com>
2024-05-18 13:50:29 +0100

    Add mkdir and build place in `build.sh` shell script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 # To build the plugin and place it in your Studio plugins folder
 # Run sh build.sh in your terminal
+mkdir build
+rojo build place.project.json --output build/hoarcekat-dev.rbxl
 rojo build plugin.project.json --plugin hoarcekat.rbxm
